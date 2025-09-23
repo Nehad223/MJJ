e38d002fb5c91fc5b0b459c8f01a3f01e681c50c
nehad222 <nihadshrith@gmail.com>
2025-09-23 16:20:43 +0300

    sdasx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // مهم لحتى ما يعطيك 404 بعد الرفع
+  base: './',
+  plugins: [react()],
 })
